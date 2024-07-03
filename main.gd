3d5ed838981ff1ebdaaabde1c119398388d3ba59
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-07-04 11:50:04 +1200

    stuff

--- main.gd
+++ main.gd
@@ -2,7 +2,7 @@ extends Node
 
 #preload obstacles
 var bush_scene = preload("res://bush.tscn")
-var wood_scene = preload("res://bush.tscn")
+var wood_scene = preload("res://wood.tscn")
 var bird_scene = preload("res://bird.tscn")
 var obstacle_types := [bush_scene, wood_scene]
 var obstacles : Array
@@ -11,6 +11,8 @@ var bird_heights := [200, 390]
 #game variables
 const DINO_START_POS := Vector2i(150, 485)
 const CAM_START_POS := Vector2i(576, 324)
+var difficulty
+const MAX_DIFFICULTY : int = 2
 var score : int
 const SCORE_MODIFIER : int = 10
 var speed : float
@@ -26,6 +28,7 @@ var last_obs
 func _ready():
 	screen_size = get_window().size
 	ground_height = $ground.get_node("Sprite2D").texture.get_height()
+	$gameover.get_node("Button").pressed.connect(new_game)
 	new_game()
 
 func new_game():
@@ -33,6 +36,8 @@ func new_game():
 	score = 0
 	show_score()
 	game_running = false
+	get_tree().paused = false
+	difficulty = 0
 	
 	#reset the nodes
 	#$dino.position = DINO_START_POS
@@ -40,15 +45,18 @@ func new_game():
 	#$Camera2D.position = CAM_START_POS
 	#$ground.position = Vector2(0, 0)
 	
-	#reset hud
+	#reset hud adn game over scene
 	$HUD.get_node("startlabel").show()
+	$gameover.hide()
 
 #Called every frame. 'delta' is the elapsed time since the previous frame
 func _process(delta):
 	if game_running:
+		#speed up and adjust difficulty
 		speed = START_SPEED + score / SPEED_MODIFIER
 		if speed > MAX_SPEED:
 			speed = MAX_SPEED
+		adjust_difficulty()
 		
 		#generate obstacles
 		generate_obs()
@@ -64,6 +72,11 @@ func _process(delta):
 		#update ground position
 		if $Camera2D.position.x - $ground.position.x > screen_size.x * 1:
 			$ground.position.x += screen_size.x
+			
+		#remove obstacles that have gone off screen
+		for obs in obstacles:
+			if obs.position.x < ($Camera2D.position.x - screen_size.x):
+				remove_obs(obs)
 	else:
 		if Input.is_action_pressed("ui_accept"):
 			game_running = true
@@ -74,7 +87,7 @@ func generate_obs():
 	if obstacles.is_empty() or last_obs.position.x < score + randi_range(300, 500):
 		var obs_type = obstacle_types[randi() % obstacle_types.size()]
 		var obs
-		var max_obs = 3
+		var max_obs = difficulty + 1
 		for i in range(randi() % max_obs + 1):
 			obs = obs_type.instantiate()
 			var obs_height = obs.get_node("Sprite2D").texture.get_height()
@@ -83,12 +96,38 @@ func generate_obs():
 			var obs_y : int = screen_size.y - ground_height - (obs_height * obs_scale.y / 2) + 5
 			last_obs = obs
 			add_obs(obs, obs_x, obs_y)
+		#additionally random chance to spawn a bird
+		if difficulty == 0: #MAX_DIFFICULTY:
+			if (randi() % 2) == 0:
+				#generate bird obstacles
+				obs = bird_scene.instantiate()
+				var obs_x : int = screen_size.x + score + 100
+				var obs_y : int = bird_heights[randi() % bird_heights.size()]
+				add_obs(obs, obs_x, obs_y)
 
 func add_obs(obs, x, y):
 	obs.position = Vector2i(x, y)
+	obs.body_entered.connect(hit_obs)
 	add_child(obs)
 	obstacles.append(obs)
+
+func remove_obs(obs):
+	obs.queue_free()
+	obstacles.erase(obs)
 	
+func hit_obs(body):
+	if body.name == "dino":
+		game_over()
 
 func show_score():
 	$HUD.get_node("scorelabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
+
+func adjust_difficulty():
+	difficulty = score / SPEED_MODIFIER
+	if difficulty > MAX_DIFFICULTY:
+		difficulty = MAX_DIFFICULTY
+
+func game_over():
+	get_tree().paused = true
+	game_running = false
+	$gameover.show()
